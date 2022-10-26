98246edbfe8e9dbe6c07bf38cd87669b747f56e3
Mahesh414 <85504221+Mahesh414@users.noreply.github.com>
2022-10-26 16:29:12 +0530

    Update sns.py

--- sns/sns.py
+++ sns/sns.py
@@ -1,51 +1,52 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-# =============================================================================
-# Created By  : Jeromie Kirchoff
-# Created Date: Mon Aug 02 17:46:00 PDT 2018
-# =============================================================================
-# Imports.
-# =============================================================================
+import json
 import smtplib
 
-# =============================================================================
-# SET EMAIL LOGIN REQUIREMENTS
-# =============================================================================
-gmail_user = 'chennamahesh.inbox@gmail.com'
-gmail_app_password = 'Chenna@1009S'
+
+
+def lambda_handler(event, context):
+    # TODO implement
+    send_email()
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
+
+
 
 # =============================================================================
-# SET THE INFO ABOUT THE SAID EMAIL
+# SEND EMAIL FUNCTION
 # =============================================================================
-sent_from = gmail_user
-sent_to = ['chennamahesh.inbox@gmail.com', 'chennamahesh414@gmail.com']
-sent_subject = "Hey Friends!"
-sent_body = ("Hey, what's up? friend!\n\n"
-             "I hope you have been well!\n"
-             "\n"
-             "Cheers,\n"
-             "Jay\n")
+def send_email():
+    # Change the items with: ######Change Me#######
+    gmail_user = 'chennamahesh.inbox@gmail.com'
+    gmail_app_password = "Chenna@1009S"
+    sent_from = gmail_user
+    sent_to = ['chennamahesh.inbox@gmail.com', 'chennamahesh414@gmail.com']
+    sent_subject = "Hello World"
+    sent_body = "Its me World"
 
-email_text = """\
+
+
+   email_text = """\
 From: %s
 To: %s
 Subject: %s
-
 %s
 """ % (sent_from, ", ".join(sent_to), sent_subject, sent_body)
 
-# =============================================================================
-# SEND EMAIL OR DIE TRYING!!!
-# Details: http://www.samlogic.net/articles/smtp-commands-reference.htm
-# =============================================================================
 
-try:
-    server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-    server.ehlo()
-    server.login(gmail_user, gmail_app_password)
-    server.sendmail(sent_from, sent_to, email_text)
-    server.close()
 
-    print('Email sent!')
-except Exception as exception:
-    print("Error: %s!\n\n" % exception)
+   try:
+        server = smtplib.SMTP_SSL('smtp.gmail.com:587')
+        server.ehlo()
+        server.login(gmail_user, gmail_app_password)
+        server.sendmail(sent_from, sent_to, email_text.encode("utf-8"))
+        server.close()
+        print(email_text)
+        print('Email sent!')
+    except Exception as exception:
+        print("Error: %s!\n\n" % exception)
+# =============================================================================
+# END OF SEND EMAIL FUNCTION
+# =============================================================================
+send_email()
