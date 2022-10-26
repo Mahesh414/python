86c688ec542fc839d7ea631f46e1a12ca83e855e
Mahesh414 <85504221+Mahesh414@users.noreply.github.com>
2022-10-26 14:31:58 +0530

    Update sns.py

--- sns/sns.py
+++ sns/sns.py
@@ -1,36 +1,51 @@
-import json
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# =============================================================================
+# Created By  : Jeromie Kirchoff
+# Created Date: Mon Aug 02 17:46:00 PDT 2018
+# =============================================================================
+# Imports
+# =============================================================================
 import smtplib
 
 # =============================================================================
-# SEND EMAIL FUNCTION
+# SET EMAIL LOGIN REQUIREMENTS
 # =============================================================================
-def send_email():
-    # Change the items with: ######Change Me#######
-    gmail_user = 'chennamahesh.inbox@gmail.com'
-    gmail_app_password = "Chenna@1009S"
-    sent_from = gmail_user
-    sent_to = ['chennamahesh.inbox@gmail.com', '######Change Me#######']
-    sent_subject = "Hello World"
-    sent_body = "Its me World"
+gmail_user = 'chennamahesh.inbox@gmail.com'
+gmail_app_password = 'Chenna@1009S'
 
-    email_text = """\
+# =============================================================================
+# SET THE INFO ABOUT THE SAID EMAIL
+# =============================================================================
+sent_from = gmail_user
+sent_to = ['chennamahesh.inbox@gmail.com', 'chennamahesh414@gmail.com']
+sent_subject = "Hey Friends!"
+sent_body = ("Hey, what's up? friend!\n\n"
+             "I hope you have been well!\n"
+             "\n"
+             "Cheers,\n"
+             "Jay\n")
+
+email_text = """\
 From: %s
 To: %s
 Subject: %s
+
 %s
 """ % (sent_from, ", ".join(sent_to), sent_subject, sent_body)
 
-    try:
-        server = smtplib.SMTP_SSL('gmail.com', 465)
-        server.ehlo()
-        server.login(gmail_user, gmail_app_password)
-        server.sendmail(sent_from, sent_to, email_text.encode("utf-8"))
-        server.close()
-        print(email_text)
-        print('Email sent!')
-    except Exception as exception:
-        print("Error: %s!\n\n" % exception)
 # =============================================================================
-# END OF SEND EMAIL FUNCTION.
+# SEND EMAIL OR DIE TRYING!!!
+# Details: http://www.samlogic.net/articles/smtp-commands-reference.htm
 # =============================================================================
-send_email()
+
+try:
+    server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+    server.ehlo()
+    server.login(gmail_user, gmail_app_password)
+    server.sendmail(sent_from, sent_to, email_text)
+    server.close()
+
+    print('Email sent!')
+except Exception as exception:
+    print("Error: %s!\n\n" % exception)
